d0aaa42432463c7ac781e61aeb8709a91f957900
Kai Lin <kailin@aidkit.cloud>
2025-07-18 00:12:00 -0500

    Updated Tailwind and css config

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useAuth } from '@/contexts/AuthContext';
+import { useAuth } from '../contexts/AuthContext';
 
 export const HomePage: React.FC = () => {
   const { user } = useAuth();
